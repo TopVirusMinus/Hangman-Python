f0358344a67f0823d42941c387a56e0ca9864578
Mustafa Marzouk <mwmma5000@gmail.com>
2020-11-05 15:30:00 +0000

    Now You Can Guess and Display Your Guess Condition

--- main.py
+++ main.py
@@ -3,16 +3,26 @@ import words
 
 words= words.words()
 wordCollection= list(words.split())
-
 randomNumber= random.randint(0, len(wordCollection)-1)
 chosenWord= wordCollection[randomNumber]
-
-guessedLetter=""
+unknownWord=""
 
 print(chosenWord)
 
-
 for letter in range(len(chosenWord)):
-  guessedLetter+= "*"
+  unknownWord+= "*"
 
-print(guessedLetter)
+print(unknownWord)
+
+
+guessedLetter= input("Enter Your Guess: ")
+if guessedLetter in chosenWord:
+     guessedIndex = chosenWord.index(guessedLetter)
+     unknownLst= list(unknownWord)
+     unknownLst[guessedIndex] = chosenWord[guessedIndex]
+     updatedUnkown= "".join(unknownLst)
+     print(updatedUnkown)
+     print("Correct guess!")
+else:
+  print("Wrong Guess!")
+    
